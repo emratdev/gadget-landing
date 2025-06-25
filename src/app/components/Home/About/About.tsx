6f63a9f6eeeccdc865d29cf30bcb51aaa9706648
emrat <mirdjalaloff@mail.ru>
2025-06-25 20:07:37 +0500

    about, review

---
+++ src/app/components/Home/About/About.tsx
@@ -0,0 +1,76 @@
+import Image from 'next/image';
+import React from 'react'
+import { FaCheck } from 'react-icons/fa';
+
+
+type Props = {
+  imageOrder: string;
+  textOrder: string;
+  title: string;
+  linkText: string;
+};
+
+const About = ({imageOrder, linkText, textOrder, title}:Props) => {
+  return (
+    <div className="pt-16 pb-16 ">
+      <div className="w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-12">
+        {/* Text Content */}
+        <div className={`${textOrder}`}>
+          {/* Title Content */}
+          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold">
+            {title}
+          </h1>
+          {/* List */}
+          <div className="mt-8">
+            <div className="flex items-center mb-6 space-x-4">
+              <div className="w-6 h-6 flex items-center justify-center flex-col rounded-full bg-pink-600 text-white">
+                <FaCheck />
+              </div>
+              <p className="text-sm sm:text-base text-gray-600 dark:text-gray-300">
+                High-Resolution Audio compatible
+              </p>
+            </div>
+
+            <div className="flex items-center mb-6 space-x-4">
+              <div className="w-6 h-6 flex items-center justify-center flex-col rounded-full bg-pink-600 text-white">
+                <FaCheck />
+              </div>
+              <p className="text-sm sm:text-base text-gray-600 dark:text-gray-300">
+                High quality wireless audio with BLUETOOTH® and LDAC technology
+              </p>
+            </div>
+
+            <div className="flex items-center mb-6 space-x-4">
+              <div className="w-6 h-6 flex items-center justify-center flex-col rounded-full bg-pink-600 text-white">
+                <FaCheck />
+              </div>
+              <p className="text-sm sm:text-base text-gray-600 dark:text-gray-300">
+                Smart listening experience by Adaptive Sound Control
+              </p>
+            </div>
+
+            <div className="flex items-center mb-6 space-x-4">
+              <div className="w-6 h-6 flex items-center justify-center flex-col rounded-full bg-pink-600 text-white">
+                <FaCheck />
+              </div>
+              <p className="text-sm sm:text-base text-gray-600 dark:text-gray-300">
+                Ergonomic, enfolding design earpads
+              </p>
+            </div>
+            {/* Link */}
+            <p className="font-semibold text-pink-600 cursor-pointer hover:underline w-fit">
+              {linkText}
+            </p>
+          </div>
+        </div>
+        {/* Image content */}
+        <div className={`${imageOrder}`}>
+            <Image src="/images/h1.png" alt="image" width={380} height={380}/>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default About
---
+++ src/app/components/Home/Review/ReviewCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image';
+import { BsQuote } from 'react-icons/bs';
+
+type Props = {
+    image: string;
+    name: string;
+    role: string;
+}
+
+const ReviewCard = ({image, name, role}:Props) => {
+  return (
+    <div>
+      <BsQuote className="w-14 h14 text-pink-500 text-5xl"/>
+      <p className='text-center text-gray-800 dark:text-gray-200 text-sm'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+        dignissimos consequuntur nisi voluptatibus ullam quod natus similique
+        saepe laborum deserunt, porro nihil eos, tempore aliq uid. Lorem ipsum,
+        dolor sit amet consectetur adipisicing elit. Facere accusantium debitis
+        mollitia nihil voluptas dolorem.
+      </p>
+      <div className='mt-6'>
+        <Image src={image} alt="image" width={100} height={100} className="object-center mx-auto rounded-full"/>
+        <h1 className='text-lg font-bold text-center mt-4'>{name}</h1>
+        <h1 className='text-center text-gray-500'>{role}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default ReviewCard
